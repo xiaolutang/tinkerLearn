3fc43c07948cc650004856e27a966b48a22dfe1a
tangxiaolu <tangxiaolu@chinamcloud.com>
2020-12-07 10:17:49 +0800

    title:引入官方demo。build.gradle

--- app/src/main/java/com/txl/tinkerlearn/app/MyApplicationLike.java
+++ app/src/main/java/com/txl/tinkerlearn/app/MyApplicationLike.java
@@ -1,5 +1,12 @@
 package com.txl.tinkerlearn.app;
 
+import android.app.Application;
+import android.content.Intent;
+
+import com.tencent.tinker.anno.DefaultLifeCycle;
+import com.tencent.tinker.entry.DefaultApplicationLike;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
+
 /**
  * Copyright (c) 2020 唐小陆 All rights reserved.
  * author：txl
@@ -9,5 +16,8 @@ package com.txl.tinkerlearn.app;
 @DefaultLifeCycle(
         application = "com.txl.tinkerlearn.app.SampleApplication",             //application name to generate
         flags = ShareConstants.TINKER_ENABLE_ALL)
-public class MyApplicationLike extends DefaultApplicationLike{
+public class MyApplicationLike extends DefaultApplicationLike {
+    public MyApplicationLike(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent) {
+        super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime, applicationStartMillisTime, tinkerResultIntent);
+    }
 }
