0d8d12cc897c06a293c2cb7f381f851deadb295e
xiaoluTang <2935347599@qq.com>
2020-12-07 21:19:22 +0800

    title:copy官方示例

--- app/src/main/java/com/txl/tinkerlearn/app/MainActivity.java
+++ app/src/main/java/com/txl/tinkerlearn/app/MainActivity.java
@@ -1,8 +1,10 @@
-package com.txl.tinkerlearn;
+package com.txl.tinkerlearn.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.txl.tinkerlearn.R;
 
 public class MainActivity extends AppCompatActivity {
 
